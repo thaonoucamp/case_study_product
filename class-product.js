4ae8d458b8700d76f5202273c52339cb0fe29e18
unknown <dangxuanthaoht@gmail.com>
2020-12-03 21:29:05 +0700

    changedSecond

--- class-product.js
+++ class-product.js
@@ -21,7 +21,10 @@ class Product {
     }
 }
 let Iphone = new Product('Iphone 12','$1000');
+document.write(Iphone.name +': co gia la : ' +Iphone.price+"<br>");
 let Mac = new Product('Macbook Air','$2000');
+document.write(Mac.name +': co gia la : ' +Mac.price+"<br>");
 let Air = new Product('Airport Pro','$100');
+document.write(Air.name +': co gia la : ' +Air.price+"<br>");
 let Ipad = new Product('Ipad Mini','$500');
-document.write(Air.name +': co gia la : ' +Air.price);
+document.write(Ipad.name +': co gia la : ' +Ipad.price);

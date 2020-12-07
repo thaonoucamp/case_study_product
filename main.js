2bb67f74cdaa8164f0697a72f65122728e4ea93c
unknown <dangxuanthaoht@gmail.com>
2020-12-07 11:26:21 +0700

    changed 7

--- main.js
+++ main.js
@@ -36,12 +36,12 @@ function addNewProduct() {
 }
 function editProduct(index) {
     // B1: Truy cap den vi tri san pham can sua;
-    let newName = prompt("Hay sua lai san pham :",product[index]);
+    let newName = prompt("Hay sua lai san pham :",product[index].);
     // B2: Gan gia tri moi cho vi tri;
     product[index] = newName;
     // B3: Hien thi lai danh sach;
     showAllProduct();
-}
+}name
 function deleteProduct(index) {
     // B1: Tim vi tri spham can xoa;
     product.splice(index,1);
